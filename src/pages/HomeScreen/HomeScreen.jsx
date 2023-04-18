b74c11366cb2f3fc656f7322a161d35836f2686c
dahalaazad <aazad.dahal@gmail.com>
2023-04-18 16:27:37 +0545

    feat: Add placeholder HomeScreen

---
+++ src/pages/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,9 @@
+const HomeScreen = () => {
+  return (
+    <div className="flex bg-slate-400 text-white justify-center h-screen">
+      <p className="font-bold align-middle">HomeScreen</p>
+    </div>
+  );
+};
+
+export default HomeScreen;
